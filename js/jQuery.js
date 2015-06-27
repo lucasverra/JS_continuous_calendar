1cbd3aa23c4b37bf6fd623ad8c6e0c5afcd5cd00
greggocabral <cabral.gr@gmail.com>
2015-06-27 20:31:23 -0300

    Incorporar logica completa de todas las semanas del proximo anio

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
 
 	// RENDERIZAR CALENDARIO VACIO CADA VEZ QUE SE CARGA LA PAGINA (ESTATICO)
+	for (var s=1; s<53; s++){
+		var id = "semana"+s;
+		$('.tabla-casilleros').append('<div id="'+id+'" class="row"></div>');
 
-	for (var i=0; i<7; i++){
-	 		var dia = getMonday(new Date())+i;
-	 		$('#semana1').append('<div class="col-xs-2 casillero-dia">'+dia+'</div>');
+		for (var i=1+(s-1)*7; i<8+(s-1)*7; i++){
+		 		var dia = moment().startOf('week').add(i,"days").format("DD");
+		 		if (dia=='01'){
+		 			var month = moment().startOf('week').add(i,"days").format("MMMM");
+		 			dia = '1 ' + month;
+		 		}
+		  		$("#semana"+s).append('<div class="col-xs-2 casillero-dia">'+dia+'</div>');
+		}
 	}
+		
 
 
 	// PEDIR BASE DE DATOS EN JSON A SERVER CON HTTP GET
@@ -19,14 +28,10 @@ $(document).ready(function() {
 
 });
 
-
-function getMonday(d) { 
-		// d = new Date(d); 
-		var day = d.getDay(); 
-		var diff = d.getDate() - day + (day == 0 ? -6:1); 
-
-		// adjust when day is sunday 
-		return diff; 
+function getMonday() { 
+		
+		var monday = moment().startOf('week').add(1,"days").format("DD");
+		return monday; 
 } 
 
-
+           
